58f841b24674b80368394baf20375ea004b1ad39
mazza <konstantinmaca@gmail.com>
2026-03-03 06:22:45 +0500

    refactor: Refactor code generally
    Refactor code in files `handlers/custom_control.rb` and
    `handlers/commands.rb`

--- app/bot/handlers/custom_control.rb
+++ app/bot/handlers/custom_control.rb
@@ -15,13 +15,14 @@ class App
 
   def handle_command_mycommands(message)
     log.debug 'handle_command_mycommands'
+
     user = User.find_by tg_user_id: message.from.id
-    if user.commands.empty?
-      bot.api.answer_message message, reply: true, text: 'You have no commands. You can create one with command /add'
-    else
-      commands = user.commands.map { it.key }.join(', ')
-      bot.api.answer_message message, reply: true, text: "Your commands: #{commands}"
-    end
+    text =
+      if user.commands.empty? then 'You have no commands. You can create one with command /add'
+      else "Your commands: #{user.commands.map { it.key }.join(', ')}"
+      end
+
+    bot.api.answer_message message, reply: true, text: text
   end
 
   def handle_command_commands(message)
@@ -49,23 +50,28 @@ class App
     return bot.api.answer_message message, reply: true, text: 'You have no commands' if user.commands.empty?
 
     if message.text =~ %r{/remove\s+(.+)$}
-      key = Regexp.last_match(1).strip.downcase
-      if (command = user.commands.find { it.key == key })
-        command.destroy
-        return bot.api.answer_message message, reply: true, text: "Command \"#{command.key}\" removed"
-      else
-        bot.api.answer_message message, reply: true, text: 'No such command'
+      if (command = user.commands.find { it.key == Regexp.last_match(1).strip.downcase })
+        return handle_command_remove_key message, command
       end
+
+      bot.api.answer_message message, reply: true, text: 'No such command'
     end
 
     user.update state: STATE_REMOVE
 
-    markup = {
-      keyboard: user.commands.map { [it.key] },
+    bot.api.answer_message message, text: 'Send me a key of a command to remove or /cancel',
+                                    reply_markup: remove_command_reply_markup(user)
+  end
+
+  def remove_command_reply_markup(user)
+    { keyboard: user.commands.map { [it.key] },
       resize_keyboard: true,
       one_time_keyboard: true,
-      selective: true
-    }.to_json
-    bot.api.answer_message message, text: 'Send me a key of a command to remove or /cancel', reply_markup: markup
+      selective: true }.to_json
+  end
+
+  def handle_command_remove_key(message, command)
+    command.destroy
+    bot.api.answer_message message, reply: true, text: "Command \"#{command.key}\" removed"
   end
 end
